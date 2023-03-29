cf9b033dd85d27ef260e680a587cf408ab97cffb
Meysam P. Ganji <p.g.meysam@gmail.com>
2023-03-29 13:24:41 +0330

    add filter example

--- src/main/scala/examples/SemiSparkSum.scala
+++ src/main/scala/examples/SemiSparkSum.scala
@@ -8,9 +8,11 @@ object SemiSparkSum {
 
     val rdd1 = sparkContext.parallelize(Range(0, 1000), 10)
     val rdd2 = rdd1.map(_ * 2)
+    val rdd3 = rdd1.filter(_ % 2 == 0)
     val result = rdd2.reduce(_ + _)
 
     println(result)
     println(rdd2.count())
+    println(rdd3.count())
   }
 }
